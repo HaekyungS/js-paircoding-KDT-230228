cdbdfb53390cd791463604e42653793ecb0a3571
KwakYounHo <kwakyh951@gmail.com>
2023-02-28 17:56:11 +0900

    밝은 테마, 다크 모드 js파일 간추리기

--- White.js
+++ White.js
@@ -1,48 +1,22 @@
-// console.log(time)
-const morning=new Date().getHours();
-if(morning>=6&&morning<=18){
-const trainingData = {
-  title: "Lorem Ipsum",
-  paragraph: "",
-  color: ["#1E1E1E", "#FFFFFF"]
-}
-
-document.body.style.cssText="width:430px; height:932px; background-color:" + trainingData.color[1] + ";"
-
-const root = document.createElement("div")
-root.setAttribute("id","root")
-root.style.cssText = "width:430px; height:932px; background-color:" + trainingData.color[1] + "; display:flex; flex-direction:column; place-content:center; align-items:center"
-document.body.appendChild(root)
-
-for(i=0;i<3;i++){
-  const divs=document.createElement("div")
-  if(i===0){
-    divs.innerHTML=trainingData.title;
-    divs.style.cssText="width:199px; height:39px; border-bottom:2px solid #000000; font-family:'Inter'; font-weight:400; font-size:24px; text-align:center"
-  }else if(i===1){
-    trainingData.paragraph="  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, debitis aut ex architecto iure similique veritatis esse eum! Iusto asperiores laboriosam aperiam fugit numquam, soluta ex debitis nesciunt. Repellendus error quos beatae fugit. Reiciendis consequatur, recusandae voluptatum maiores sunt eius quia inventore, sed cum nobis, a odio. Dolores quod aliquid dicta molestias modi veniam eaque quasi nobis unde delectus ipsa, quis ipsum in recusandae expedita fuga voluptates ea tempore eius magni id quibusdam culpa sed.";
-    divs.innerHTML=trainingData.paragraph
-    divs.style.cssText="width:295px; height:186px; font-family:'Inter'; font-weight:400; font-size:12px; margin-top:22px; margin-bottom:123px;"
-  }else{
-    divs.style.cssText="width:82px; height:82px; background-color:"+trainingData.color[0]+"; border-radius:41px;"
-  }
-  root.appendChild(divs)
-}
-
-
-// const CreateTag = {
-//   tagName: "",
-//   AttriName: "",
-//   AttriValue: "",
-//   Parent: "",
-//   Makers: function () {
-//     const element = document.createElement(this.tagName)
-//     this.parent.appendChild(element)
-//   }
-// }
-// console.dir(document.body)
-// CreateTag.tagName = "div"
-// CreateTag.Parent = root
-// CreateTag.Makers()
-
+if (time===1) {
+  /*-----------상수 선언----------*/
+  const root = document.getElementById('root');
+  const header = document.getElementById('header');
+  const main = document.getElementById('main');
+  const footer = document.getElementById('footer');
+  const title = header.children[0];
+  const mainText = main.children[0]
+  const circle = footer.children[0];
+  
+  /*-----------화이트모드-----------*/
+  // root.style.backgroundColor = '#383838';
+  
+  // 제목 글씨 색상
+  title.style.color = 'black';
+  
+  // 메인 글씨 색상
+  mainText.style.color = 'black';
+  
+  // 버튼 색상
+  circle.style.backgroundColor='#5a5a5a';
 }
